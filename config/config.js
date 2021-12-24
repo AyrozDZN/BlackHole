4f297f39d96d0a3129b9d348c2d15b65f1992db4
Thibaut Breton <tbreton@et.esiea.fr>
2021-12-24 13:06:15 +0100

    📝 Envoie d'un message au créateur du serveur et à la personne qui a ajouté le serveur pour config le bot

--- config/config.js
+++ config/config.js
@@ -1,14 +1,12 @@
-//rename this file config.js
-
 module.exports = {
     emojis: {
         
-    }, //list of emoji for easy use in the code
+    },
 
     discord: {
-        token: 'OTIyODA5MTgyMjI5NDk5OTI1.YcG3Bg.nHSJsvP_15zLsHcwzeMboWhUqlw', //token of your bot
-        color : '#f8ab37', //color of main embed
-        colorError : '#F04946', //color of error embed
-        colorSuccess : '#43B481' //color of success embed
+        token: 'OTIyODA5MTgyMjI5NDk5OTI1.YcG3Bg.nHSJsvP_15zLsHcwzeMboWhUqlw',
+        color : '#b90234',
+        colorError : '#F04946',
+        colorSuccess : '#43B481'
     }
 };
---
+++ events/guildCreate.js
@@ -0,0 +1,32 @@
+const discord = require('discord.js')
+
+module.exports = async (client, guild) => {
+    
+    if (guild.available == false) return;
+    
+    const audit = (await guild.fetchAuditLogs()).entries.first()
+    if (audit.action === "BOT_ADD") {
+
+        const setupEmbed = new discord.MessageEmbed()
+            .setAuthor("BlackHole", client.user.avatarURL(), "https://blackhole.net")
+            .setTitle("SETUP")
+            .setDescription("To setup the bot, you have to do the command `/setup` on your server, only an administrator or the creator will be able to execute this command")
+            .setColor(client.config.discord.color)
+            .setTimestamp()
+            .setFooter("BlackHole - Protect your server", client.user.avatarURL())
+
+        if (audit.executor.id == guild.ownerId) {
+            
+            const channel = await audit.executor.createDM()
+            channel.send({ content: `<@${audit.executor.id}>`, embeds: [setupEmbed] })
+        
+        } else {
+            
+            const channelAuditExecutor = audit.executor.createDM()
+            const channelOwner = guild.members.cache.get(guild.ownerId).user.createDM()
+            channelAuditExecutor.send({ content: `<@${audit.executor.id}>`, embeds: [setupEmbed] })
+            channelOwner.send({ content: `<@${guild.ownerId}>`, embeds: [setupEmbed] })
+        
+        }
+    }
+};
