8a591bf2f3a30f639bb8f9eb504ebd5ff4bc7361
Thibaut Breton <thibaut53.breton@gmail.com>
2021-12-24 10:41:12 +0100

    Update and rename config.example.js to config.js

--- config/config.js
+++ config/config.js
@@ -6,7 +6,7 @@ module.exports = {
     }, //list of emoji for easy use in the code
 
     discord: {
-        token: 'Type your token here', //token of your bot
+        token: 'OTIyODA5MTgyMjI5NDk5OTI1.YcG3Bg.ZnZBqO1fDv-oq8lUEpnnIoHxFRI', //token of your bot
         color : '#f8ab37', //color of main embed
         colorError : '#F04946', //color of error embed
         colorSuccess : '#43B481' //color of success embed
